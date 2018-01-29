175420ecb4afde129f4c68e4177f62dc591bb5ef
龙拳 <itachimood@163.com>
2018-01-29 10:57:45 +0800

    Initial Commit

---
+++ StackViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackViewDemo
+//
+//  Created by 冯振伟 on 2018/1/29.
+//  Copyright © 2018年 冯振伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
